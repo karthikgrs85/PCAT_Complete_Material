bbd5d2a5f63056aa9d82e89879609561120e79f5
karthikgrs85 <166644264+karthikgrs85@users.noreply.github.com>
2024-05-06 14:55:51 +0530

    Add files via upload

---
+++ Code/FunctionPtrAndCallback/Arithmetic.c
@@ -0,0 +1,18 @@
+#include "Arithmetic.h"
+
+float add(float x, float y)
+{
+	return (x+y);
+}
+float subtract(float x, float y)
+{
+	return (x-y);
+}
+float multiply(float x, float y)
+{
+	return (x*y);
+}
+float divide(float x, float y)
+{
+	return (x/y);
+}
