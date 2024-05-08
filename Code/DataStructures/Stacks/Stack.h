9194eba957cd88634e76643201a8b87a7abc9b25
karthikgrs85 <166644264+karthikgrs85@users.noreply.github.com>
2024-05-08 17:24:18 +0530

    Add files via upload

---
+++ Code/DataStructures/Stacks/Stack.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define STACK_MAX 50
+
+//LIFO
+typedef struct
+{
+	char *buf[STACK_MAX];
+	int top;
+
+} Stack_string; 
+
+//Get the last inserted element in the Stack
+char* peek(const Stack_string* Ss);
+
+//Push an element buf into the Stack
+int push(Stack_string* Ss, const char buf[]);
+
+//Pop the last inserted element out of the Stack
+char* pop(Stack_string *Ss);
+
+//Print all the elements of the Stack
+void printAll(const Stack_string* Ss);
