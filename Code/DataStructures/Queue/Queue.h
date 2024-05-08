f717def3c520ecda6c6c1f40089ac7d2e4652d91
karthikgrs85 <166644264+karthikgrs85@users.noreply.github.com>
2024-05-08 17:23:51 +0530

    Add files via upload

---
+++ Code/DataStructures/Queue/Queue.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define QUEUE_MAX 50
+
+//FIFO 
+typedef struct
+{
+	char* buf[QUEUE_MAX];
+	int start, end;
+
+} Queue_String;
+
+//Add an element buf to the beginning of the Queue
+int enqueue(Queue_String *Qs, const char buf[]);
+
+//Remove the first inserted element from the Queue
+char* dequeue(Queue_String *Qs);
+
+//Print the elements in the Queue
+int printQueue(const Queue_String *Qs);
